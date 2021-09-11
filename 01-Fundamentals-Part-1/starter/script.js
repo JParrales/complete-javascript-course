3f0a6641f4bda7a011363c265187102a52135221
Jose <josedparrales@gmail.com>
2021-09-11 16:19:30 -0500

    20. TYpe COnversion and Coercion

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,143 @@
+/*
+let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+// Variable name conventions
+let firtName = "Matilda";
+
+console.log(firtName);
+console.log(firtName);
+console.log(firtName);
+
+let PI = 3.1415
+PI = 2
+console.log(PI)
+*/
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun);
+
+let year;
+console.log(year)
+console.log(typeof year)
+
+year = 1991
+console.log(typeof year);
+
+console.log(ty)
+*/
+
+// let age = 10;
+// age = 31;
+
+// const birthYear = 1991;
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// let x = 10 + 5;
+// x += 10;
+// console.log(x);
+
+// let x, y;
+// x = y = 13;
+// console.log(x, y)
+
+/* 
+const marksWeights = 78;
+const marksHeights = 1.69;
+const johnWeights = 92;
+const johnHeights = 1.95;
+
+let bmiMarks = marksWeights / marksHeights ** 2
+let bmijohn = johnWeights / johnHeights ** 2
+let markHigherBMI = bmiMarks > bmijohn
+
+console.log(bmiMarks, bmijohn, markHigherBMI) 
+*/
+/* 
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+
+const year = 2037;
+
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(jonasNew);
+
+console.log('String with \n\
+multiple \n\
+lines')
+console.log(`String
+multiple
+lines`)
+ */
+
+const age = 15;
+// const isOldEnough = age >= 18;
+/* 
+if (age >= 18) {
+  console.log("Sara can start driving license ");
+} else {
+  const yearsLest = 18 - age;
+  console.log(`Sara is too young. Wait another ${yearsLest} years :) `);
+}
+
+const birthYear = 1991;
+let century
+
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century)
+
+ */
+/* 
+const marksWeights = 78;
+const marksHeights = 1.69;
+const johnWeights = 92;
+const johnHeights = 1.95;
+
+let bmiMarks = marksWeights / marksHeights ** 2
+let bmijohn = johnWeights / johnHeights ** 2
+let markHigherBMI = bmiMarks > bmijohn
+
+let out
+if (bmiMarks > bmijohn) {
+  out = `Mark's BMI (${bmiMarks}) is higher than John's (${bmijohn})!`
+} else {
+  out = `John's BMI (${bmijohn})  is higher than Mark's (${bmiMarks})!`
+}
+
+console.log(out)
+
+ */
+
+/* const inputYear = "1991";
+console.log(Number(inputYear))
+console.log(Number(inputYear) + 18);
+ */
+
+// type coercion
+
+console.log('I am ' +23 + ' years old')
